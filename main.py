d51c515ec20fe681222ec6c0238a8fc4557a1bb7
Matuz Bence <matuzb97@gmail.com>
2022-04-05 12:08:39 +0200

    forecast

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from datetime import date
 
-import yfinances as yf
+import yahoo_finance as yf
 from fbprophet import Prophet
 from fbprophet.plot import plot_plotly
 from  plotly import graph_obj as go
@@ -31,5 +31,41 @@ def load_data(ticker):
 data_load_state = st.text("load data...")
 
 data = load_data(selected_stocks)
-data_load_state.text("loadin done")
+data_load_state.text("loading done")
 
+st.subheader('Raw data')
+st.write(data.tail())
+
+
+def plot_raw_data():
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name= 'Stock_open'))
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['close'], name= 'Stock_close'))
+    fig.layout.update(title_text="Time series data", xaxis_rangelider_visible = True)
+    st.plotly_chart(fig)
+
+
+plot_raw_data()
+
+#forecast
+
+df_train = data[['Date', 'Close']]
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)
+
+st.subheader('Forecast data')
+st.write(forecast.tail())
+
+
+st.write('Forecast data')
+fig1 = plot_plotly(m, forecast)
+
+st.plotly_chart(fig1)
+
+st.write('Forecast component')
+fig2= m.plot_components(forecast)
+st.write(fig2)
